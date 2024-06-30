0709da43eb125bdfa00d41247a505670ac340bf2
Carlos Henrique <ichfs9108@gmail.com>
2024-06-30 15:38:31 -0300

    feat: Implementando payment-topic KafkaAdmin

--- payment-service/src/main/java/com/valdir/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/valdir/payment_service/PaymentServiceApplication.java
@@ -3,6 +3,7 @@ package com.valdir.payment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PaymentServiceApplication {
 
